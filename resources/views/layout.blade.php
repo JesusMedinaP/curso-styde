605719a71f303d96dcae3318c926caf0adc24bc9
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-01-18 11:40:09 +0100

    Intento de Bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,18 +8,15 @@
     <meta name="generator" content="Hugo 0.108.0">
     <title>@yield('title') - Styde.net</title>
     <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sticky-footer-navbar/">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">-->
+    <link href="{{ asset('public/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!--<link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sticky-footer-navbar/">-->
+   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>-->
+    <script src="{{ asset('public/js/bootstrap.min.js') }}"></script>
 
     <!-- Favicons -->
     <link rel="icon" href="public/favicon.ico">
-    <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
     <!--<link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json"-->
-    <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
     <meta name="theme-color" content="#712cf9">
 
 
