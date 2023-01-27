a236d4c00694d305165e5f1fb91b8e0a8db86a83
JesusMedinaP <jesusmedinaparraga@gmail.com>
2023-01-27 11:50:04 +0100

    Hasta el vídeo 26

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,6 +5,9 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-end mb-3">
         <h1 class="pb-1">{{ $title }}</h1>
+        <div class="form-check form-check-inline align-items-end">
+            <a href="{{ route('users.create') }}" class="btn btn-dark">Nuevo usuario</a>
+        </div>
     </div>
 
     @include('users._filters')
@@ -17,8 +20,10 @@
                 <tr>
                     <th scope="col"># <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
                     <th scope="col" class="sort-desc">Nombre <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
-                    <th scope="col">Correo <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
-                    <th scope="col">Fechas <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                    <th scope="col">Empresa<span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                    <th scope="col">Correo<span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                    <th scope="col">Rol<span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+                    <th scope="col">Fechas<span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
                     <th scope="col" class="text-right th-actions">Acciones</th>
                 </tr>
                 </thead>
